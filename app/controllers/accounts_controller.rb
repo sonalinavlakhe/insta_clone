de38e5fd54718815a706fa9b40b2f800d4f2fc6c
sonali <sonalibhavsar977@gmail.com>
2020-06-11 11:20:23 +0530

    Add view profile functionality

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,12 +1,17 @@
 class AccountsController < ApplicationController
   before_action :authenticate_account!
-  
+  before_action :set_account, only: [:profile]
   
   def index
-    #user feed
+    #user dashboard -post feed
+    @posts = Post.active
   end
 
-  def show
+  def profile
+    @posts = @account.posts.active
+  end
 
+  def set_account
+    @account = Account.find_by_username(params[:username])
   end
 end
