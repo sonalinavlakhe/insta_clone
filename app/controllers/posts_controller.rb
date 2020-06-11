fef4a6e0076aa762edd0c80e34e72512cdaa2b62
sonali <sonalibhavsar977@gmail.com>
2020-06-11 14:28:39 +0530

    Add description below post and suggestions section

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:image, :image_cache)
+    params.require(:post).permit(:image, :image_cache, :description)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,11 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :account
+  default_scope {order created_at: :desc}
+
   scope :active, -> {where active: true}
+
+  def total_likes
+    0
+  end
 end
