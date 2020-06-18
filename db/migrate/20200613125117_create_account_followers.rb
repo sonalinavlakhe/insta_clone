d2cbcaad5dc9c7a4c69ab6253b71b232cd3c3a14
sonali <sonalibhavsar977@gmail.com>
2020-06-18 11:05:03 +0530

    Add new migration

---
+++ db/migrate/20200613125117_create_account_followers.rb
@@ -0,0 +1,9 @@
+class CreateAccountFollowers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :account_followers do |t|
+      t.references :follower, references: :accounts
+      t.references :following, references: :accounts
+      t.timestamps
+    end
+  end
+end
