9481e5a7508684c6283e70aeddb9a0473fe54278
sonali <sonalibhavsar977@gmail.com>
2020-06-18 11:23:49 +0530

    Add profile_picture and can_edit_profile?

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,2 +1,10 @@
 module AccountsHelper
+  def profile_picture(account, width = 100)
+    image_path = account.image.present? ? account.image.thumb.url: "placeholder-profile.jpg"
+    image_tag(image_path, width: width, class: "img-circle")
+  end
+
+  def can_edit_profile? profile_id
+    account_signed_in? && current_account.id == profile_id
+  end
 end
