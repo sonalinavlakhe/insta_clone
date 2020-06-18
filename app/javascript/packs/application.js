5d8ff995027a30475625afc723941e81caaf4a05
sonali <sonalibhavsar977@gmail.com>
2020-06-18 11:27:30 +0530

    Addconfig changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,16 +8,16 @@
 //= require jquery_ujs
 //= require_tree .
 
+window.jQuery = $;
+window.$ = $;
 
-
-require("jquery.min")
 require("@rails/ujs").start() 
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require("jquery_ujs")
-require("bootstrap.min")
+require("packs/posts")
+
 
 
 
