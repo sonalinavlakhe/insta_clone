966002f2284a74a0e62e656669f9e2e06684970c
sonali <sonalibhavsar977@gmail.com>
2020-06-18 11:21:29 +0530

    Add follow_account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,10 +1,25 @@
 class AccountsController < ApplicationController
   before_action :authenticate_account!
   before_action :set_account, only: [:profile]
+
   
   def index
     #user dashboard -post feed
     @posts = Post.active
+    @comment = Comment.new
+    following_ids = AccountFollower.where(follower_id: current_account.id).map(&:following_id)
+    following_ids << current_account.id
+    @follower_suggestions = Account.where.not(id: following_ids)
+  end
+
+  def follow_account
+    follower_id = params[:follow_id]
+    if AccountFollower.create!(follower_id: current_account.id, following_id: follower_id)
+      flash[:success] = "Now following user"
+    else
+      flash[:danger] = "Unable to add followers"
+    end
+    redirect_to dashboard_path
   end
 
   def profile
