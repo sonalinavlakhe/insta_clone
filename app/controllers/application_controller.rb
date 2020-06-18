4428c83dee3592f952f0a66d21949c6b3fdec1a2
sonali <sonalibhavsar977@gmail.com>
2020-06-18 11:21:57 +0530

    add after_sign_in_path_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :image,:image_cache, :description, :website])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :firstname, :lastname, :email, :password])
   end
+
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
 end
